7bd0945ddf1a631912d9043c3866ca50dc3eefb2
Alexander Romanov <ralex4edu@gmail.com>
2026-04-04 17:46:19 +0300

    fix(admin): remove user & groups setting

--- total_recall/trainer/admin.py
+++ total_recall/trainer/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 from .models import Collection, Progress, Word
+from django.contrib.auth.models import User, Group
 
 admin.site.register(Collection)
 admin.site.register(Word)
 admin.site.register(Progress)
+admin.site.unregister(User)
+admin.site.unregister(Group)
